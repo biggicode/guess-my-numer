e156143d1f36cb2bea11a717f64ceef1bf16f866
Lari Oncescu <larioncescu@yahoo.com>
2021-12-12 17:56:57 +0000

    add 2 if statements to display losing

--- script.js
+++ script.js
@@ -7,16 +7,25 @@ document.querySelector('.check').addEventListener('click', () => {
   let userInput = Number(document.querySelector('.guess').value);
 
   if (!userInput) {
-    if (userInput === randomNumber) {
-      document.querySelector('.message').textContent = 'Yeeey!👏 You won!🎉';
-    } else if (userInput > randomNumber) {
-      document.querySelector('.message').textContent = 'Keep trying!!🎆To high';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else if (userInput < randomNumber) {
-      document.querySelector('.message').textContent = 'To Loooooow! 🎇';
-      score--;
-      document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent =
+      "This isn't a valid number!☢";
+  } else if (userInput === randomNumber) {
+    document.querySelector('.message').textContent = 'Yeeey!👏 You won!🎉';
+  } else if (userInput > randomNumber) {
+    document.querySelector('.message').textContent = 'Keep trying!!🎆To high';
+    score--;
+    document.querySelector('.score').textContent = score;
+    if (score === 0) {
+      document.querySelector('.message').textContent =
+        "You lost the battle! Don't be sad! Win the war🎉";
+    }
+  } else if (userInput < randomNumber) {
+    document.querySelector('.message').textContent = 'To Loooooow! 🎇';
+    score--;
+    document.querySelector('.score').textContent = score;
+    if (score === 0) {
+      document.querySelector('.message').textContent =
+        "You lost the battle! Don't be sad! Win the war🎉";
     }
   }
   console.log(userInput);
